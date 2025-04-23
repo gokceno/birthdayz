98a41b2aed769a97ef5fe899f13a1e283365091e
Gokcen Ogutcu <gokcen@brewww.com>
2025-04-23 06:21:16 +0300

    chore: Add scheduled job execution support

--- src/commands/send.ts
+++ src/commands/send.ts
@@ -1,4 +1,5 @@
 import { command, string } from "@drizzle-team/brocli";
+import { Cron } from "croner";
 import { yaml as config } from "../utils/config";
 import { logger } from "../utils/logger";
 import { DateTime } from "luxon";
@@ -21,47 +22,50 @@ const send = command({
   },
   handler: async (opts) => {
     const env: Config = config(opts["config-file"]);
+    logger.info(`Job started.`);
     logger.debug(`Fetched ${env.team.length} record(s).`);
-    const now = DateTime.local();
-    const birthdays: TeamMember[] = env.team.filter(
-      ({ birthdate }: TeamMember) => {
-        return (
-          now.toFormat("MM-dd") ===
-          DateTime.fromISO(birthdate).toFormat("MM-dd")
-        );
-      },
-    );
-    logger.info(
-      `Found ${birthdays.length} birthday(s) for ${now.toFormat("MM-dd")}.`,
-    );
-    for (const member of birthdays) {
-      logger.info(`Found ${member.fullName}`);
-      const pickedTemplateParts: Code = await pick(member.email);
-      if (!pickedTemplateParts) {
-        logger.warn(
-          `Sending for ${member.email} failed. Either ran out of codes or already sent this year.`,
-        );
-        continue;
+    new Cron(env.schedule, {}, async () => {
+      const now = DateTime.local();
+      const birthdays: TeamMember[] = env.team.filter(
+        ({ birthdate }: TeamMember) => {
+          return (
+            now.toFormat("MM-dd") ===
+            DateTime.fromISO(birthdate).toFormat("MM-dd")
+          );
+        },
+      );
+      logger.info(
+        `Found ${birthdays.length} birthday(s) for ${now.toFormat("MM-dd")}.`,
+      );
+      for (const member of birthdays) {
+        logger.info(`Found ${member.fullName}`);
+        const pickedTemplateParts: Code = await pick(member.email);
+        if (!pickedTemplateParts) {
+          logger.warn(
+            `Sending for ${member.email} failed. Either ran out of codes or already sent this year.`,
+          );
+          continue;
+        }
+        const templateParams: Template = {
+          firstname: member.fullName.split(" ")[0],
+          ...pickedTemplateParts,
+        };
+        logger.debug(JSON.stringify(templateParams));
+        try {
+          const body: string = createPayload(env.mail, member, templateParams);
+          const mjResponse = await mjSend({
+            ...env.mail,
+            body,
+          });
+          logger.debug(body);
+          logger.debug(JSON.stringify(mjResponse));
+          logger.info(`Message sent to ${member.email}`);
+        } catch (e: any) {
+          logger.error(e.message);
+        }
+        await burn(pickedTemplateParts.code, member.email);
       }
-      const templateParams: Template = {
-        firstname: member.fullName.split(" ")[0],
-        ...pickedTemplateParts,
-      };
-      logger.debug(JSON.stringify(templateParams));
-      try {
-        const body: string = createPayload(env.mail, member, templateParams);
-        const mjResponse = await mjSend({
-          ...env.mail,
-          body,
-        });
-        logger.debug(body);
-        logger.debug(JSON.stringify(mjResponse));
-        logger.info(`Message sent to ${member.email}`);
-      } catch (e: any) {
-        logger.error(e.message);
-      }
-      await burn(pickedTemplateParts.code, member.email);
-    }
+    });
   },
 });
 
